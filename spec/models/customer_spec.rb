20d2fe64d265b7b3365f30eced530f8fb01066b7
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-29 06:58:45 -0700

    start building customer model tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Customer do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a first name" do
+        customer = Customer.new(last_name: "Casimir")
+
+        expect(customer).to be_invalid
+      end
+
+      it "is invalid without a last_name" do
+        customer = Customer.new(first_name: "Jeff")
+
+        expect(customer).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a credit card number and result" do
+        customer = create(:customer)
+        merchant = create(:merchant)
+        invoice = customer.invoices.create(customer_id: customer.id, merchant_id: merchant.id, status: "filled")
+        customer = invoice.customers.create(credit_card_number: "1234", result: "shipped", invoice_id: invoice.id)
+
+        expect(customer).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "belongs to an invoice" do
+      customer = create(:customer)
+      merchant = create(:merchant)
+      invoice = customer.invoices.create(customer_id: customer.id, merchant_id: merchant.id, status: "filled")
+      customer = invoice.customers.create(credit_card_number: "1234", result: "shipped", invoice_id: invoice.id)
+
+      expect(customer).to respond_to(:invoice)
+    end
+  end
 end
