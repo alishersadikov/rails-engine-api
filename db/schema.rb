258ba07e8af546dfb9c03e21dc9143fa4ced08d2
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-28 18:17:24 -0700

    create relevant models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129000151) do
+ActiveRecord::Schema.define(version: 20161129011352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,44 @@ ActiveRecord::Schema.define(version: 20161129000151) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "invoices", force: :cascade do |t|
+    t.integer  "customer_id"
+    t.integer  "merchant_id"
+    t.string   "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["customer_id"], name: "index_invoices_on_customer_id", using: :btree
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id", using: :btree
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "unit_price"
+    t.integer  "merchant_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["merchant_id"], name: "index_items_on_merchant_id", using: :btree
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string   "credit_card_number"
+    t.string   "credit_card_expiration_date"
+    t.string   "result"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "invoice_id"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id", using: :btree
+  end
+
+  add_foreign_key "invoices", "customers"
+  add_foreign_key "invoices", "merchants"
+  add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
---
+++ spec/factories/transactions.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :transaction do
+    credit_card_number "MyString"
+    credit_card_expiration_date "MyString"
+    result "MyString"
+  end
+end
