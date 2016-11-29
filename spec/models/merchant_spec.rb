df56fe30003747ec3fb0c91bd76c177372c5392a
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-29 06:32:03 -0700

    build model tests for merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
-RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Merchant do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        merchant = Merchant.new
+
+        expect(merchant).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        merchant = Merchant.new(name: "some merchant")
+
+        expect(merchant).to be_valid
+      end
+    end
+
+    context "uniqueness" do
+      it "is invalid if name is not unique" do
+        Merchant.create(name: "some merchant")
+        merchant = Merchant.new(name: "some merchant")
+
+        expect(merchant).to be_invalid
+      end
+
+      it "is valid if name is unique" do
+        Merchant.create(name: "some merchant")
+        merchant = Merchant.new(name: "other merchant")
+
+        expect(merchant).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many items" do
+      merchant = Merchant.new(name: "some merchant")
+
+      expect(merchant).to respond_to(:items)
+    end
+
+    it "has many invoices" do
+      merchant = Merchant.new(name: "some merchant")
+
+      expect(merchant).to respond_to(:invoices)
+    end
+
+    it "has many customers through invoices" do
+      merchant = Merchant.new(name: "some merchant")
+
+      expect(merchant).to respond_to(:customers)
+    end
+  end
 end
