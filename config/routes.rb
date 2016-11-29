81d1a43c65b5bd30fff617a5f4b195acc8150c2b
Megan Talbot <meganftalbot@gmail.com>
2016-11-29 16:59:49 -0700

    Add merchant relationship for showing all items for one merchant

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
+        get '/:id/items', to: 'items#index'
       end
 
       namespace :customers do
