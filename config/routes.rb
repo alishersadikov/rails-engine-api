4c9261ec1666605996fdb18feb47bdda559e8e65
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-29 13:07:10 -0700

    find functionality complete

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,13 @@ Rails.application.routes.draw do
 
 
 
-      
+      namespace :merchants do
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+        get '/random', to: 'random#show'
+      end
+
       resources :merchants, only: [:index, :show]
-
-
     end
   end
 end
