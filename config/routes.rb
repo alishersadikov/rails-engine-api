53324ad15741e0ee89a7d9f2ad8bbfdfcd8e6654
Megan Talbot <meganftalbot@gmail.com>
2016-11-29 12:06:27 -0700

    Add random and find_all for items record endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       namespace :items do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
+        get '/random', to: 'random#show'
       end
       resources :items, only: [:index, :show]
     end
