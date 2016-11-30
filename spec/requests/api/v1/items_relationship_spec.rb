ca8cc072ec537fc0759fa74b0c37a5936f97ac18
alishersadikov <alishersadikov@gmail.com>
2016-11-30 00:36:49 -0700

    complete item relationships

---
+++ spec/requests/api/v1/items_relationship_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "item relationships" do
+  context "GET /api/v1/items/:id/invoice_items" do
+    it "returns all associated invoice_items with one item" do
+      item = create(:item_with_merchant)
+      invoice_item_1 = create(:invoice_item_with_invoice, item_id: item.id)
+      invoice_item_2 = create(:invoice_item_with_invoice, item_id: item.id)
+
+      get "/api/v1/items/#{item.id}/invoice_items"
+
+      parsed_invoice_items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(parsed_invoice_items.first["id"]).to eq(invoice_item_1.id)
+      expect(parsed_invoice_items.first["invoice_id"]).to eq(invoice_item_1.invoice_id)
+      expect(parsed_invoice_items.last["id"]).to eq(invoice_item_2.id)
+      expect(parsed_invoice_items.last["invoice_id"]).to eq(invoice_item_2.invoice_id)
+    end
+  end
+
+  context "GET /api/v1/items/:id/merchant" do
+    it "returns the associated merchant with one item" do
+      item = create(:item_with_merchant)
+
+      get "/api/v1/items/#{item.id}/merchant"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant["id"]).to eq(item.merchant.id)
+      expect(merchant["name"]).to eq(item.merchant.name)
+    end
+  end
+end
