9d203471d93da2483c0c664ba69574eaa5711fed
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-30 00:36:49 -0700

    complete item relationships

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
+        get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/merchant', to: 'merchants#show'
       end
 
       resources :items, only: [:index, :show]
