a5ecd11b298e5fd7b10d713dc3199b332adca0b0
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-30 12:41:29 -0700

    complete favorite customer

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -15,4 +15,8 @@ class Merchant < ApplicationRecord
     revenue = invoices.joins(:invoice_items, :transactions).merge(Transaction.successful).sum("invoice_items.quantity * invoice_items.unit_price")
     { revenue: revenue }
   end
+
+  def favorite_customer
+    customers.joins(:transactions).merge(Transaction.successful).group(:id, :first_name, :last_name).order("count(customers.id) DESC").first
+  end
 end
