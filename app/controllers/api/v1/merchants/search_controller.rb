eb0c07692ff8a74eaa7bd5fa9c7283d8a0a2d51c
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-30 02:06:57 -0700

    implement case insensitive search for merchant name

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,15 +1,29 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    render json: Merchant.find_by(merchant_params)
+    merchant = Merchant.find_by(merchant_params)
+    return render json: merchant if merchant
+    return case_insensitive_search(:one_record)
   end
 
   def index
-    render json: Merchant.where(merchant_params)
+    merchants = Merchant.where(merchant_params)
+    return render json: merchants if !merchants.empty?
+    return case_insensitive_search(:all_records)
   end
 
   private
 
   def merchant_params
     params.permit(:id, :name, :created_at, :updated_at)
+  end
+
+  def case_insensitive_search(type_of_search)
+    attribute = merchant_params.keys.first
+    t = Merchant.arel_table
+    if type_of_search == :one_record
+      render json: Merchant.find_by(t[attribute].matches(merchant_params[attribute]))
+    else
+      render json: Merchant.where(t[attribute].matches(merchant_params[attribute]))
+    end
   end
 end
