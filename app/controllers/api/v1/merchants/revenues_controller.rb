0cb3dbaef82d454c056da18df59d4b02eebb60df
Megan Talbot <meganftalbot@gmail.com>
2016-11-30 15:29:56 -0700

    Add merchant business relationships revenue by date, passing spec harness

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::RevenuesController < ApplicationController
   def show
-    render json: Merchant.find(params[:id]).revenue
+    render json: Merchant.find(params[:id]).revenue(params[:date])
   end
 end
