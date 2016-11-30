5f0490d27aa8e17a7f3f5b1e9e22936416c800a5
alishersadikov <alishersadikov@gmail.com>
2016-11-30 00:18:02 -0700

    complete invoiceitem relationships

--- app/controllers/api/v1/invoice_items/invoices_controller.rb
+++ app/controllers/api/v1/invoice_items/invoices_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::InvoiceItems::InvoicesController < ApplicationController
   def show
-    render json: InvoiceItem.find_by(invoice_id: params[:id]).invoice
+    render json: InvoiceItem.find(params[:id]).invoice
   end
 end
