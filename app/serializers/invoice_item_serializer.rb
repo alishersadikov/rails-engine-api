efbb8cb151ebf6618c9da40f3871d469e9b67af0
Megan Talbot <meganftalbot@gmail.com>
2016-12-01 16:06:18 -0700

    Fix unit price search

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -2,7 +2,7 @@ class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :invoice_id, :item_id, :quantity, :unit_price
 
   def unit_price
-   "#{(object.unit_price/100.0)}"
+   "#{(object.unit_price/100.00)}"
   end
-  
+
 end
--- app/serializers/total_revenue_serializer.rb
+++ app/serializers/total_revenue_serializer.rb
@@ -2,7 +2,6 @@ class TotalRevenueSerializer < ActiveModel::Serializer
   attributes :total_revenue
 
   def total_revenue
-
     "#{object/100.00}"
   end
 
