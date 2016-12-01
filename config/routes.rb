23e471f10159b2ae9ca0064571d021f823d1b79d
alishersadikov <alishersadikov@gmail.com>
2016-12-01 01:44:46 -0700

    complete favorite merchant

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ Rails.application.routes.draw do
         get '/random', to: 'random#show'
         get '/:id/invoices', to: 'invoices#index'
         get '/:id/transactions', to: 'transactions#index'
+        get '/:id/favorite_merchant', to: 'favorites#show'
 
       end
 
