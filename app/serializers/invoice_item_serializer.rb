8d8776ddd89eeff877cd6e9902f55be9a1e8edb4
Megan Talbot <meganftalbot@gmail.com>
2016-11-30 20:34:57 -0700

    Add method to convert to currency with two decimal places for prices

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,3 +1,8 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :invoice_id, :item_id, :quantity, :unit_price
+
+  def unit_price
+   "#{(object.unit_price/100.0)}"
+  end
+  
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,8 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :description, :id, :merchant_id, :name, :unit_price
 
+  def unit_price
+   "#{(object.unit_price/100.0)}"
+  end
+
 end
