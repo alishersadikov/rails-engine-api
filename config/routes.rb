ec8b8ba335f09e596cdac5e166f243e1dc546bc5
Megan Talbot <meganftalbot@gmail.com>
2016-11-30 17:12:52 -0700

    Add total revenue across all merchants for one date

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
+        get '/revenue', to: 'revenues#index'
         get '/:id/items', to: 'items#index'
         get '/:id/invoices', to: 'invoices#index'
         get '/:id/revenue', to: 'revenues#show'
