53fb83548944788b5358a9a137d8d5fe319e7b4d
Megan Talbot <meganftalbot@gmail.com>
2016-11-30 17:12:52 -0700

    Add total revenue across all merchants for one date

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,14 +12,12 @@ class Merchant < ApplicationRecord
   end
 
   def revenue(date = nil)
-
     if date == nil
       revenue = invoices.joins(:invoice_items, :transactions)
       .merge(Transaction.successful)
       .sum("invoice_items.quantity * invoice_items.unit_price")
       { revenue: revenue }
     else
-
       revenue = invoices.joins(:invoice_items, :transactions)
       .merge(Transaction.successful)
       .where(created_at: date)
@@ -28,7 +26,26 @@ class Merchant < ApplicationRecord
     end
   end
 
+  def self.total_revenue(date)
+    revenue = Invoice.all.where(created_at: date)
+    .joins(:invoice_items, :transactions)
+    .merge(Transaction.successful)
+    .sum("invoice_items.quantity * invoice_items.unit_price")
+
+    # revenue = joins(:invoices)
+    # select("invoices.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    # .joins(:transactions, :invoice_items)
+    # .merge(Transaction.successful)
+    # .where(created_at: date)
+    # .group("invoices.id")
+
+    { total_revenue: revenue }
+  end
+
   def favorite_customer
-    customers.joins(:transactions).merge(Transaction.successful).group(:id, :first_name).order("count(customers.id) DESC").first
+    customers.joins(:transactions)
+    .merge(Transaction.successful)
+    .group(:id, :first_name)
+    .order("count(customers.id) DESC").first
   end
 end
