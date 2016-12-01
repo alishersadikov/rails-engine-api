c60b45ff5b93344030c8c66be644033d8eee487c
alishersadikov <alishersadikov@gmail.com>
2016-12-01 00:19:05 -0700

    changed migrations for citext

--- db/migrate/20161129010100_create_items.rb
+++ db/migrate/20161129010100_create_items.rb
@@ -1,12 +1,16 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
+    enable_extension 'citext'
+
     create_table :items do |t|
-      t.string :name
+      t.citext :name
       t.text :description
       t.integer :unit_price
       t.references :merchant, foreign_key: true
 
       t.timestamps
     end
+
+    # add_index :items, :name, unique: true
   end
 end
