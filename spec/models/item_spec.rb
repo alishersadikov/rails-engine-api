d431d1e0ef0e869f99f145b3f7b73611f2d0ff83
Megan Talbot <meganftalbot@gmail.com>
2016-12-01 17:42:52 -0700

    Complete model testing for Item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -60,4 +60,46 @@ describe Item do
       expect(item).to respond_to(:invoices)
     end
   end
+
+  describe ".best_day" do
+    it "returns the best day for the item" do
+      date = "2016-12-01T17:30:21.051Z"
+      item = create(:item_with_merchant)
+      invoice_1 = create(:invoice_with_transactions, created_at: date)
+      invoice_2 = create(:invoice_with_transactions, created_at: date)
+      invoice_item_1 = create(:invoice_item, invoice: invoice_1, item: item)
+      invoice_item_2 = create(:invoice_item, invoice: invoice_2, item: item)
+      invoice_item_3 = create(:invoice_item, invoice: invoice_2, item: item)
+
+      expect(item.best_day).to eq(date)
+    end
+  end
+
+  describe ".most_revenue" do
+    it "returns the top items ranked by total revenue" do
+      item_1, item_2 = create_list(:item_with_merchant, 2)
+      invoice_1 = create(:invoice_with_transactions)
+      invoice_2 = create(:invoice_with_transactions)
+      invoice_item_1 = create(:invoice_item, invoice: invoice_1, item: item_1)
+      invoice_item_2 = create(:invoice_item, invoice: invoice_2, item: item_2)
+      invoice_item_3 = create(:invoice_item, invoice: invoice_2, item: item_2)
+
+      expect(Item.most_revenue.first.name).to eq(item_2.name)
+      expect(Item.most_revenue.last.name).to eq(item_1.name)
+    end
+  end
+
+  describe ".most_items" do
+    it "returns the top items ranked by total number sold" do
+      item_1, item_2 = create_list(:item_with_merchant, 2)
+      invoice_1 = create(:invoice_with_transactions)
+      invoice_2 = create(:invoice_with_transactions)
+      invoice_item_1 = create(:invoice_item, invoice: invoice_1, item: item_1)
+      invoice_item_2 = create(:invoice_item, invoice: invoice_2, item: item_2)
+      invoice_item_3 = create(:invoice_item, invoice: invoice_2, item: item_2)
+
+      expect(Item.most_items.first.name).to eq(item_2.name)
+      expect(Item.most_items.last.name).to eq(item_1.name)
+    end
+  end
 end
