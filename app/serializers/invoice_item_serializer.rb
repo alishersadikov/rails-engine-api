59ba467981094cb746a997a0b4ae599265b613e5
alishersadikov <alishersadikov@gmail.com>
2016-12-02 03:33:04 -0700

    fix decimal point bugs in all serializers

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -2,7 +2,6 @@ class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :invoice_id, :item_id, :quantity, :unit_price
 
   def unit_price
-   "#{(object.unit_price/100.00)}"
+    sprintf "%.2f", "#{(object.unit_price/100.00)}"
   end
-
 end
