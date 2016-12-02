2f1375dc468e4598332784f8d7cb8192c76b83ea
alishersadikov <alishersadikov@gmail.com>
2016-12-02 04:08:17 -0700

    add minor refactors to relationship tests

--- spec/requests/api/v1/relationships/merchant_relationships_spec.rb
+++ spec/requests/api/v1/relationships/merchant_relationships_spec.rb
@@ -4,33 +4,34 @@ describe 'merchants relationships' do
   context 'GET /api/v1/merchants/:id/items' do
     it 'returns a list of all items associated with one merchant' do
       merchant = create(:merchant)
-      item = merchant.items.create(name: "ItemName", description: "Beauituful item!",  unit_price: 5, merchant_id: merchant.id)
-      item2 = merchant.items.create(name: "ItemName2", description: "Beauituful item!",  unit_price: 7, merchant_id: merchant.id)
+
+      item_1, item_2 = create_list(:item, 2, merchant: merchant)
 
       get "/api/v1/merchants/#{merchant.id}/items"
 
       items = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(merchant.items.count).to eq(2)
-      expect(merchant.items.first["name"]).to eq(item.name)
+      expect(items.count).to eq(2)
+      expect(items.first["id"]).to eq(item_1.id)
+      expect(items.first["id"]).to eq(item_1.id)
+      expect(items.last["id"]).to eq(item_2.id)
     end
   end
 
   context 'GET /api/v1/merchants/:id/invoices' do
     it 'returns a list of all invoices associated with one merchant' do
-      customer = create(:customer)
       merchant = create(:merchant)
-      invoice1 = customer.invoices.create(merchant_id: merchant.id, status:"filled")
-      invoice2 = customer.invoices.create(merchant_id: merchant.id, status:"filled")
+      invoice_1, invoice_2 = create_list(:invoice_with_customer_and_merchant, 2, merchant: merchant)
 
       get "/api/v1/merchants/#{merchant.id}/invoices"
 
-      invoices = JSON.parse(response.body)
+      parsed_invoices = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(merchant.invoices.count).to eq(2)
-      expect(merchant.invoices.first["status"]).to eq(invoice1.status)
+      expect(parsed_invoices.count).to eq(2)
+      expect(parsed_invoices.first["id"]).to eq(invoice_1.id)
+      expect(parsed_invoices.last["id"]).to eq(invoice_2.id)
     end
   end
 end
